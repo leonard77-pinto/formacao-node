c1d40d127c2c4e45f632090b12ab465cb2ac72d1
leonard77-pinto <leonard77.pinto@gmail.com>
2024-03-08 10:41:09 -0300

    test transaction sum

--- api-rest-nodejs/test/exemplo.spec.ts
+++ api-rest-nodejs/test/exemplo.spec.ts
@@ -51,3 +51,58 @@ test('list transactions', async ()=>{
     //console.log(r.get('Set-Cookie'))
     //expect(r.statusCode).toEqual(201)
 })
+
+test('get transaction id', async ()=>{
+    const r =  await request(app.server)
+        .post('/transactions')
+        .send({
+            title: 'teste 1',
+            amount: 200,
+            type: 'credit'
+        })
+    
+    const l = await request(app.server)
+        .get('/transactions')
+        .set('Cookie', r.get('Set-Cookie'))
+    
+
+    const id = l.body.ret[0].id
+
+    const t = await request(app.server)
+    .get(`/transactions/${id}`)
+    .set('Cookie', r.get('Set-Cookie'))
+    .expect(200)
+
+    expect(t.body.ret).toEqual(
+        expect.objectContaining({
+            title: 'teste 1',
+            amount: 200,
+        })
+    )
+})
+
+test('get sum', async ()=>{
+    const r =  await request(app.server)
+        .post('/transactions')
+        .send({
+            title: 'credit',
+            amount: 500,
+            type: 'credit'
+        })
+    
+    await request(app.server)
+        .post('/transactions')
+        .set('Cookie', r.get('Set-Cookie'))
+        .send({
+            title: 'debit',
+            amount: 100,
+            type: 'debit'
+        })
+
+    const l = await request(app.server)
+        .get('/transactions/sum')
+        .set('Cookie', r.get('Set-Cookie'))
+        .expect(200)
+    
+    expect(l.body.ret.amount).toEqual(400)
+})
